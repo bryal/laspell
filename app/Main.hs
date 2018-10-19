39dce6221e6783f2e8759e0bec1438d631cc7670
Johan Johansson <96.bryal@gmail.com>
2018-10-19 13:55:25 +0200

    initial commit

---
+++ app/Main.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Lib
+import System.Environment
+
+help = "Laspell - A preprocessor for Haskell to use LISP syntax.\n"
+       ++ "USAGE: laspell ORIGINAL FROM TO\n"
+       ++ "  where ORIGINAL is the original filename;\n"
+       ++ "        FROM is the file to read from;\n"
+       ++ "        TO is the file to write to."
+
+preprocessFile from to = do
+  src <- readFile from
+  writeFile to (preprocess src)
+
+main :: IO ()
+main = getArgs >>= (\args -> if length args == 3
+                             then preprocessFile (args !! 1) (args !! 2)
+                             else putStrLn ("Error: Expects 3 args.\n" ++ help))
---
+++ src/Lib.hs
@@ -0,0 +1,5 @@
+module Lib where
+
+preprocess src = src ++ "\n\nfoo = 1337"
+
+
